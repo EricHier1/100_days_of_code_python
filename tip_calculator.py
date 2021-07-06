8dbe431a3e7c96e2d8203b76aa57fe9bd456225b
Eric <erichierholzer1@gmail.com>
2021-07-05 22:01:23 -0700

    Update tip_calculator.py
    used "{:.2f}".format() on line 9 to ensure two decimal places displayed even when second decimal is a 0

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,10 +1,11 @@
+print("Welcome to the tip calculator")
 bill = input("How much is the bill? $")
-tip = input("What is the percent of the tip (5, 10, 15, 20, ect.)?")
+tip = int(input("What is the percent of the tip(5, 10, 15, 20)? "))
 tip_percent = float(tip) / 100
-persons = input("How many people are splitting the bill?")
+persons = int(input("How many people are splitting the bill?"))
 
 cost_per_person = (float(bill) / float(persons)) * ((1 + tip_percent))
 
-rounded_cost_person = round(cost_per_person,2)
+rounded_cost_person = "{:.2f}".format(round(cost_per_person,2))
 
 print(f"Each person should pay ${rounded_cost_person}")
